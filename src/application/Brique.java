41de426210d9cccca7a37d8063b9cc4eaf43151e
yoann <>
2022-04-20 11:43:46 +0200

    MAJ: Auto-format Brique

--- src/application/Brique.java
+++ src/application/Brique.java
@@ -4,19 +4,18 @@ import javafx.scene.paint.Color;
 
 public class Brique {
 
-	public int hauteur, largeur, longueur,x,y,z;
+	public int hauteur, largeur, longueur, x, y, z;
 	public Color couleur;
 
-	public Brique(int hauteur, int largeur, int longueur, Color couleur,int x, int y, int z) {
+	public Brique(int hauteur, int largeur, int longueur, Color couleur, int x, int y, int z) {
 		this.hauteur = hauteur;
 		this.largeur = largeur;
 		this.longueur = longueur;
 		this.couleur = couleur;
-		this.x=x;
-		this.y=y;
-		this.z=z;
-		
-		
+		this.x = x;
+		this.y = y;
+		this.z = z;
+
 	}
 
 }
