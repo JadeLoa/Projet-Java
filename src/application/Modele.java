1b6ed85f6f70a51e13d1e5e48c372c754b9a16c9
JadeLoa <81516802+JadeLoa@users.noreply.github.com>
2022-04-20 11:41:57 +0200

    Ajout : OrderBrique()

--- src/application/Modele.java
+++ src/application/Modele.java
@@ -1,6 +1,8 @@
 package application;
 
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 
 import javafx.scene.paint.Color;
 
@@ -21,8 +23,31 @@ public class Modele {
 	private LinkedList<Construction> listeConstruction;
 	private LinkedList<Brique> ordreB;
 
+	private Map<PointVue, int[]> coefVue = new HashMap<PointVue, int[]>();
+
+	public Modele() {
+		int[] a1 = { 0, -1, 0 };
+		coefVue.put(PointVue.N, a1);
+		int[] a2 = { 0, 1, 0 };
+		coefVue.put(PointVue.S, a2);
+		int[] a3 = { -1, 0, 0 };
+		coefVue.put(PointVue.E, a3);
+		int[] a4 = { 1, 0, 0 };
+		coefVue.put(PointVue.O, a4);
+		int[] a5 = { 0, 0, 1 };
+		coefVue.put(PointVue.DESSUS, a5);
+		int[] a6 = { -1, -1, 1 };
+		coefVue.put(PointVue.NE, a6);
+		int[] a7 = { 1, -1, 1 };
+		coefVue.put(PointVue.NO, a7);
+		int[] a8 = { -1, 1, 1 };
+		coefVue.put(PointVue.SE, a8);
+		int[] a9 = { 1, 1, 1 };
+		coefVue.put(PointVue.SO, a9);
+	}
+
 	void rechercherConstruction() {
-		// recré la linkedList listeConstruction par les fichiers (nomC)
+		// recrée la linkedList listeConstruction par les fichiers (nomC)
 		// on initialise constructionEnCours
 	}
 
@@ -42,6 +67,22 @@ public class Modele {
 		// Sud : Y croissant
 		// Ouest : X croissant
 		// Est : X decroissant
+		// renvoie une liste triée
+		LinkedList<Brique> listeTriee = new LinkedList<Brique>();
+		int X = coefVue.get(pdv)[0];
+		int Y = coefVue.get(pdv)[1];
+		int Z = coefVue.get(pdv)[2];
+		listeTriee.add(listeATrier.get(0));
+		for (int i = 1; i < listeATrier.size(); i++) {
+			int cpt = 0;
+			while (X * listeATrier.get(i).x > X * listeTriee.get(cpt).x
+					&& Y * listeATrier.get(i).y > Y * listeTriee.get(cpt).y
+					&& Z * listeATrier.get(i).z > Z * listeTriee.get(cpt).z //
+					&& cpt < listeTriee.size()) {
+				cpt++;
+			}
+			listeTriee.add(cpt, listeATrier.get(i));
+		}
 		return null;
 
 	}
@@ -67,8 +108,8 @@ public class Modele {
 
 	boolean ajouterConstruction(String nomNouvelleC) {
 		// vérifier que le nomConstruction n'existe pas déjà
-		// cre un fichier vide
-		// cré une nouvelle construction dans la liste des constructions
+		// crée un fichier vide
+		// crée une nouvelle construction dans la liste des constructions
 		return false; // true si on a ajouté la construction, false sinon
 	}
 
