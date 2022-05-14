90715400691f92b34754ff7be9df7d7e3ee44aad
aegrajag <99244022+aegrajag@users.noreply.github.com>
2022-05-14 16:02:10 +0200

    Update Modele.java

--- src/application/Modele.java
+++ src/application/Modele.java
@@ -1,8 +1,18 @@
 package application;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
@@ -20,36 +30,48 @@ public class Modele {
 	private PointVue pdv = PointVue.N;
 	private Color[] listeCouleurs = { Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW, Color.ORANGE, Color.VIOLET,
 			Color.BLACK, Color.GRAY };
-	private Color couleurEnCours = Color.RED;// couleur par défaut
+	private int couleurEnCours = 0; // couleur par défaut (indice)
 	private LinkedList<Construction> listeConstruction;
 	private LinkedList<Brique> ordreB;
 
 	private Map<PointVue, int[]> coefVue = new HashMap<PointVue, int[]>();
 
 	public Modele() {
-		int[] a1 = { 0, -1, 0 };
-		coefVue.put(PointVue.N, a1);
-		int[] a2 = { 0, 1, 0 };
-		coefVue.put(PointVue.S, a2);
-		int[] a3 = { -1, 0, 0 };
-		coefVue.put(PointVue.E, a3);
-		int[] a4 = { 1, 0, 0 };
-		coefVue.put(PointVue.O, a4);
-		int[] a5 = { 0, 0, 1 };
-		coefVue.put(PointVue.DESSUS, a5);
-		int[] a6 = { -1, -1, 1 };
-		coefVue.put(PointVue.NE, a6);
-		int[] a7 = { 1, -1, 1 };
-		coefVue.put(PointVue.NO, a7);
-		int[] a8 = { -1, 1, 1 };
-		coefVue.put(PointVue.SE, a8);
-		int[] a9 = { 1, 1, 1 };
-		coefVue.put(PointVue.SO, a9);
+		int[][] coefs = { { 0, -1, 0 }, { 0, 1, 0 }, { -1, 0, 0 }, { 1, 0, 0 }, { 0, 0, 1 }, { -1, -1, 1 },
+				{ 1, -1, 1 }, { -1, 1, 1 }, { 1, 1, 1 } };
+		PointVue[] povs = { PointVue.N, PointVue.S, PointVue.E, PointVue.O, PointVue.DESSUS, PointVue.NE, PointVue.NO,
+				PointVue.SE, PointVue.SO };
+		for (int i = 0; i < coefs.length; i++) {
+			coefVue.put(povs[i], coefs[i]);
+		}
 	}
 
-	void rechercherConstruction() {
+	ArrayList<String> rechercherConstruction() {
 		// recrée la linkedList listeConstruction par les fichiers (nomC)
 		// on initialise constructionEnCours
+		ArrayList<String> listResults = new ArrayList<>();
+
+		Gson gson = new Gson();
+
+		File folder = new File("BibliConstruction");
+		File[] listOfFiles = folder.listFiles();
+		this.listeConstruction = new LinkedList<>();
+
+		for (int i = 0; i < listOfFiles.length; i++) {
+			if (listOfFiles[i].isFile()) {
+				try {
+					String strFile = listOfFiles[i].getName();
+					if (strFile.endsWith(".json")) {
+						this.listeConstruction.add(gson.fromJson(new FileReader(listOfFiles[i]), Construction.class));
+						listResults.add(strFile.substring(0, strFile.length() - 5));
+					}
+				} catch (JsonSyntaxException | JsonIOException | FileNotFoundException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		System.out.print(this.listeConstruction.size());
+		return listResults;
 	}
 
 	void afficherConstruction(GraphicsContext gc) {
@@ -62,10 +84,11 @@ public class Modele {
 		// constructionEnCours
 
 		LinkedList<Brique> testL = new LinkedList<>(); // TODO renplacer avec liste de construction
-		//testL.add(new Brique(2, 3, 4, Color.BLACK, 0, 3, 0));
-		//testL.add(new Brique(1, 1, 1, Color.RED, 9, 2, 17));
-		//testL.add(new Brique(10, 1, 1, Color.GREEN, 2, 0, 0));
-		//testL.add(new Brique(2, 2, 1, Color.BLUE, 10, 1, 15));
+		/* @formatter:off
+		testL.add(new Brique(2, 3, 4, Color.BLACK, 0, 3, 0));
+		testL.add(new Brique(1, 1, 1, Color.RED, 9, 2, 17));
+		testL.add(new Brique(10, 1, 1, Color.GREEN, 2, 0, 0));
+		testL.add(new Brique(2, 2, 1, Color.BLUE, 10, 1, 15));
 
 		testL = this.ordreBrique(testL);
 
@@ -74,7 +97,7 @@ public class Modele {
 			for (int x = 0; x < briqueEnCours.largeur; x++) {
 				for (int y = 0; y < briqueEnCours.longueur; y++) {
 					for (int z = 0; z < briqueEnCours.hauteur; z++) {
-						//gc.setFill(briqueEnCours.couleur);
+						gc.setFill(briqueEnCours.couleur);
 						gc.fillRect((briqueEnCours.x + x) * 25, (briqueEnCours.z + z) * 25, briqueEnCours.largeur * 25,
 								briqueEnCours.hauteur * 25); // TODO ordre à changer en fonction de pdv
 						gc.fill();
@@ -83,6 +106,7 @@ public class Modele {
 				}
 			}
 		}
+		@formatter:on */
 	}
 
 	LinkedList<Brique> ordreBrique(LinkedList<Brique> listeATrier) {
@@ -103,7 +127,7 @@ public class Modele {
 					&& X * listeATrier.get(i).x >= X * listeTriee.get(cpt).x
 					&& Y * listeATrier.get(i).y >= Y * listeTriee.get(cpt).y
 					&& Z * listeATrier.get(i).z >= Z * listeTriee.get(cpt).z) {
-				System.out.println(i + " " + listeATrier.size());
+				// System.out.println(i + " " + listeATrier.size());
 				cpt++;
 			}
 			listeTriee.add(cpt, listeATrier.get(i));
@@ -114,11 +138,20 @@ public class Modele {
 
 	void sauvegarder(String nomSauvC) {
 		// modifie le fichier de la construction modifiée
+
 	}
 
-	void ajouterBrique(int largeur, int longueur, int hauteur, int x, int y) {
+	void ajouterBrique(int largeur, int longueur, int hauteur, int x, int y, int z) {
 		// creer brique à la première place directement au dessous possible
 		// ne pas sortir des limites
+
+		Brique newB = new Brique(largeur, longueur, hauteur, this.couleurEnCours, x, y, z);
+
+		for (int i = 0; i < this.listeConstruction.size(); i++) {
+			if (this.listeConstruction.get(i).nomConstruction == this.constructionEnCours) {
+				this.listeConstruction.get(i).listeBrique.addLast(newB);
+			}
+		}
 
 		// mise à jour du fichier listeBrique de cette construction
 
@@ -128,6 +161,12 @@ public class Modele {
 		// recherche la première brique dans la liste en partant du haut (z decroissant)
 		// et la supprime
 
+		for (int i = 0; i < this.ordreB.size(); i++) {
+			if (this.ordreB.get(i).x == x && this.ordreB.get(i).y == y) {
+				this.ordreB.remove(i);
+			}
+		}
+
 		// met la liste de brique a jour
 	}
 
@@ -135,7 +174,29 @@ public class Modele {
 		// vérifier que le nomConstruction n'existe pas déjà
 		// crée un fichier vide
 		// crée une nouvelle construction dans la liste des constructions
-		return false; // true si on a ajouté la construction, false sinon
+		if (nomNouvelleC.length() == 0) {
+			return false;
+		}
+		for (Construction c : this.listeConstruction) {
+			if (c.nomConstruction.equals(nomNouvelleC)) {
+				return false;
+			}
+		}
+		Construction newC = new Construction(nomNouvelleC);
+
+		Gson gson = new Gson();
+		String json = gson.toJson(newC);
+
+		try (FileWriter writer = new FileWriter("BibliConstruction/" + nomNouvelleC + ".json")) {
+			gson.toJson(newC, writer);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		this.listeConstruction.addLast(newC);
+		return true;
+
+		// true si on a ajouté la construction, false sinon
 	}
 
 	boolean supprimerConstruction() {
@@ -145,8 +206,7 @@ public class Modele {
 	}
 
 	void changerCouleur(int i) {
-		this.couleurEnCours = this.listeCouleurs[i];
-		System.out.print(i);
+		this.couleurEnCours = i;
 	}
 
 }
