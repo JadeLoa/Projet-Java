c5a22f04f354378a05d48bdbffcf3a45213475f1
JadeLoa <81516802+JadeLoa@users.noreply.github.com>
2022-05-16 13:52:42 +0200

    MAJ: Fonctionnalites affichage

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,6 +12,9 @@ import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
 import javafx.stage.Stage;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.scene.transform.Rotate;
 
 public class Main extends Application {
 	@Override
@@ -23,59 +26,44 @@ public class Main extends Application {
 			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-
-
-			PhongMaterial redMaterial = new PhongMaterial();
-			redMaterial.setDiffuseColor(Color.DARKRED);
-			redMaterial.setSpecularColor(Color.RED);
-		
-			Piece pieceRacine=new Piece(1, 2, 5, Color.RED);
-			Piece pieceF1=new Piece(1, 4, 2, Color.RED);
 			
-			pieceRacine.connecteursM[0][1]= new Connexion(pieceF1, new Position(0,0));
+			Piece pieceRacine, pieceF1, pieceF2;
 			
-			for (Box box1: pieceRacine.generate3DBoxes()) {
-			
-			box1.setMaterial(redMaterial);
-			root.getChildren().add(box1);
-			System.out.println(box1.getDepth()+","+box1.getHeight()+","+box1.getWidth());
-			}
-			PerspectiveCamera camera = new PerspectiveCamera();
-			camera.setTranslateX(0);
-			camera.setTranslateY(90);
-			camera.setTranslateZ(150);
-			scene.setCamera(camera);
+//          pieceRacine = new Piece(1, 8, 2, Color.RED);
+//          pieceF1 = new Piece(1, 3, 2, Color.GREEN);
+//          pieceF2 = new Piece(1, 1, 10, Color.BLUE);
+//          pieceRacine.connecteursM[1][7] = new Connexion(pieceF1, new Position(0, 0));
+//          pieceRacine.connecteursM[1][0] = new Connexion(pieceF2, new Position(0, 0));
 
-			// box2.translateXProperty().set(box2.translateXProperty().get()-25);
-			initMouseControl(scene, camera, primaryStage);
-
-			/*
-			 * PhongMaterial redMaterial = new PhongMaterial();
-			 * redMaterial.setDiffuseColor(Color.DARKRED);
-			 * redMaterial.setSpecularColor(Color.RED);
-			 * 
-			 * Piece pieceRacine=new Piece(1, 2, 5, Color.RED);
-			 * 
-			 * for (Box box1: pieceRacine.generate3DBoxes()) {
-			 * 
-			 * box1.setMaterial(redMaterial); root.getChildren().add(box1);
-			 * System.out.println(box1.getDepth()+","+box1.getHeight()+","+box1.getWidth());
-			 * } PerspectiveCamera camera = new PerspectiveCamera();
-			 * camera.setTranslateX(0); camera.setTranslateY(90); camera.setTranslateZ(150);
-			 * scene.setCamera(camera);
-			 * 
-			 * // box2.translateXProperty().set(box2.translateXProperty().get()-25);
-			 * initMouseControl(scene, camera, primaryStage);
-			 */
+//          pieceRacine = new Piece(1, 8, 4, Color.YELLOW);
+//          pieceF1 = new Piece(1, 3, 5, Color.GREEN);
+//          pieceRacine.connecteursM[1][6] = new Connexion(pieceF1, new Position(4, 1));
 
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+//          pieceRacine = new Piece(1, 8, 5, Color.YELLOW);
+//          pieceF1 = new Piece(1, 4, 5, Color.GREEN);
+//          pieceRacine.connecteursM[1][3] = new Connexion(pieceF1, new Position(0, 0));
 
-	}
+			pieceRacine = generer_demo();
+            
+            pieceRacine.generate3DBoxes().forEach(box1 -> {
+                root.getChildren().add(box1);
+            });
+            PerspectiveCamera camera = new PerspectiveCamera();
+            camera.setTranslateX(90);
+            camera.setTranslateY(90);
+            camera.setTranslateZ(-500);
+            scene.setCamera(camera);
 
-	private double anchorX, anchorY;
+            // box2.translateXProperty().set(box2.translateXProperty().get()-25);
+            initMouseControl(camera, scene, primaryStage);
+
+        } catch (Exception e) {
+        }
+
+    }
+
+/*	private double anchorX, anchorY;
 
 	private void initMouseControl(Scene scene, Camera camera, Stage stage) {
 
@@ -94,7 +82,66 @@ public class Main extends Application {
 			// box.setTranslateZ(box.getTranslateZ() + event.getDeltaY());
 		});
 	}
+*/
+	
+	
+	
+	//Tracks drag starting point for x and y
+    private double anchorX, anchorY;
+//Keep track of current angle for x and y
+    private double anchorAngleX = 0;
+    private double anchorAngleY = 0;
+//We will update these after drag. Using JavaFX property to bind with object
+    private final DoubleProperty angleX = new SimpleDoubleProperty(0);
+    private final DoubleProperty angleY = new SimpleDoubleProperty(0);
 
+    private void initMouseControl(Camera camera, Scene scene, Stage stage) {
+        Rotate xRotate;
+        Rotate yRotate;
+        camera.getTransforms().addAll(
+                xRotate = new Rotate(0, Rotate.X_AXIS),
+                yRotate = new Rotate(0, Rotate.Y_AXIS)
+        );
+        xRotate.angleProperty().bind(angleX);
+        yRotate.angleProperty().bind(angleY);
+
+        scene.setOnMousePressed(event -> {
+            anchorX = event.getSceneX();
+            anchorY = event.getSceneY();
+            anchorAngleX = angleX.get();
+            anchorAngleY = angleY.get();
+        });
+
+        scene.setOnMouseDragged(event -> {
+            angleX.set(anchorAngleX - (anchorY - event.getSceneY()));
+            angleY.set(anchorAngleY + anchorX - event.getSceneX());
+        });
+        stage.addEventHandler(ScrollEvent.SCROLL, event -> {
+            camera.setTranslateY(camera.getTranslateY() + event.getDeltaY());
+        });
+
+    }
+    
+            
+public static Piece generer_demo(){
+        Piece pieceRacine,pieceF;
+            pieceRacine = new Piece(1, 30, 30, Color.YELLOW);
+            pieceF = new Piece(1, 30, 2, Color.GREEN);
+            pieceRacine.connecteursM[0][0] = new Connexion(pieceF, new Position(0, 0));        
+            pieceF = new Piece(1, 30, 2, Color.GREEN);
+            pieceRacine.connecteursM[28][0] = new Connexion(pieceF, new Position(0, 0));        
+            pieceF = new Piece(1, 2, 26, Color.RED);
+            pieceRacine.connecteursM[2][0] = new Connexion(pieceF, new Position(0, 0));      
+            pieceF = new Piece(1, 2, 26, Color.RED);
+            pieceRacine.connecteursM[2][28] = new Connexion(pieceF, new Position(0, 0));      
+            pieceF = new Piece(1, 10, 10, Color.PURPLE);
+            pieceRacine.connecteursM[11][11] = new Connexion(pieceF, new Position(0, 0));              
+        return pieceRacine;
+        
+    }
+
+	
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
--- src/application/Piece.java
+++ src/application/Piece.java
@@ -4,10 +4,11 @@ import java.util.ArrayList;
 
 import javafx.geometry.Point3D;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
 
 public class Piece {
-	public static final double PAS=50;
+	public static final double PAS=50, PAS_H=(2*PAS)/3;
 
     public int hauteur, largeur, longueur;
     public Color couleur;
@@ -30,13 +31,19 @@ public class Piece {
     public boolean connecterPieceF(Position connecteurDepart, Piece pieceF, Position connecteursF[][]) {       
         
         // verifier s'il a pas un bug de reference de connecteur
-        if (connecteurDepart.i+connecteursF.length>=connecteursM.length) return false;
-        if (connecteurDepart.j+connecteursF[0].length>=connecteursM[0].length) return false;
+        if (connecteurDepart.i+connecteursF.length>=connecteursM.length) {
+        	return false;
+        }
+        if (connecteurDepart.j+connecteursF[0].length>=connecteursM[0].length) {
+        	return false;
+        }
         
         // verifier si les connecteurs des 2 pieces sont libres
         for (int i = 0; i <= connecteursF.length; i++) {
             for (int j = 0; j < connecteursF[i].length; j++) {
-                if (this.connecteursM[connecteurDepart.i + i][connecteurDepart.j + j]!=null) return false;
+                if (this.connecteursM[connecteurDepart.i + i][connecteurDepart.j + j]!=null) {
+                	return false;
+                }
             }
         } 
         
@@ -50,47 +57,55 @@ public class Piece {
         return true;
     }
     
-    
+   
 	Point3D calculerPosition(Piece pieceF) {
 		Point3D pF=null;
-		
-		
-		
 		return pF;
 	}
+
 	
 	
 	public ArrayList<Box> generate3DBoxes(){
 		ArrayList<Box> boxes=new ArrayList();
 		Box boxM=new Box();
-		boxM.setWidth(PAS*this.largeur);
-		boxM.setHeight(PAS*this.hauteur);
-		boxM.setDepth(PAS*this.longueur);
-		boxes.add(boxM);
-		
-		for(int i=0;i<this.connecteursM.length;i++)
-			for(int j=0;j<this.connecteursM[i].length;j++)
-			{
-				if (this.connecteursM[i][j]!=null) {
-					Piece pieceF=this.connecteursM[i][j].pieceFemelle;
-					Box boxF=new Box();
-					boxF.setWidth(PAS*pieceF.largeur);
-					boxF.setHeight(PAS*pieceF.hauteur);
-					boxF.setDepth(PAS*pieceF.longueur);	
-					int i1=this.connecteursM[i][j].connecteurF.i;
-					int j1=this.connecteursM[i][j].connecteurF.j;
-					double depx,depy,depz;
-					depy = boxM.translateYProperty().get()-PAS;
-					boxF.translateYProperty().set(depy);
-					depx = boxM.translateXProperty().get()+(i1 + pieceF.largeur/2.0 -i-this.largeur/2.0)*PAS;
-					boxF.translateXProperty().set(depx);
-					depz = boxM.translateZProperty().get()+(j1 - pieceF.longueur/2.0 +j-this.longueur/2.0)*PAS;
-					boxF.translateZProperty().set(depz);					
-					boxes.add(boxF);
-				}
-			}
-		
-		return boxes;
+		PhongMaterial material = new PhongMaterial();
+        material.setDiffuseColor(this.couleur);
+        material.setSpecularColor(this.couleur);
+        boxM.setMaterial(material);
+
+        boxM.setWidth(PAS * this.largeur);
+        boxM.setHeight(PAS_H * this.hauteur);
+        boxM.setDepth(PAS * this.longueur);
+
+        boxes.add(boxM);
+
+        for (int i = 0; i < this.connecteursM.length; i++) {
+            for (int j = 0; j < this.connecteursM[i].length; j++) {
+                if (this.connecteursM[i][j] != null) {
+                    Piece pieceF = this.connecteursM[i][j].pieceFemelle;
+                    Box boxF = new Box();
+                    boxF.setWidth(PAS * pieceF.largeur);
+                    boxF.setHeight(PAS_H * pieceF.hauteur);
+                    boxF.setDepth(PAS * pieceF.longueur);
+                    int i1 = this.connecteursM[i][j].connecteurF.i;
+                    int j1 = this.connecteursM[i][j].connecteurF.j;
+                    double depx, depy, depz;
+                    depy = boxM.translateYProperty().get() - PAS_H;
+                    boxF.translateYProperty().set(depy);
+                    depx = boxM.translateXProperty().get() + ((j - j1) + (pieceF.largeur - this.largeur) / 2.0) * PAS;
+                    boxF.translateXProperty().set(depx);
+                    depz = boxM.translateZProperty().get() + ((i1 - i) + (this.longueur - pieceF.longueur) / 2.0) * PAS;
+                    boxF.translateZProperty().set(depz);
+                    material = new PhongMaterial();
+                    material.setDiffuseColor(pieceF.couleur);
+                    material.setSpecularColor(pieceF.couleur);
+                    boxF.setMaterial(material);
+                    boxes.add(boxF);
+                }
+            }
+        }
+
+        return boxes;
 	}
 	
 	
